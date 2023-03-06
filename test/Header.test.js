6eba1657f1f5d2b7e40d1e5aa970b465405b9f36
Eddie Lao <eddielao@gmail.com>
2023-03-06 11:47:21 -0500

    Refactoring
    Always refactor tests after getting them to pass. Refactor by using beforeEach block for repated codes.

--- test/Header.test.js
+++ test/Header.test.js
@@ -5,29 +5,32 @@ import { act } from 'react-dom/test-utils';
 import { Header } from '../src/Header';
 
 describe("Header", () => {
+    let container;
+    beforeEach(() => {
+        container = document.createElement("div");
+        document.body.replaceChildren(container);
+    });
+
     it('renders my first name', () => {
         const customer = { firstName: "Eddie" };
         // const component = (<Header customer={customer} />),
-        const component = <Header customer={customer} />,
-            container = document.createElement("div");
+        const component = <Header customer={customer} />;
 
-        document.body.replaceChildren(container);
         act(() => {
             ReactDOM.createRoot(container).render(component);
         });
+
         expect(document.body.textContent).toContain('Eddie');
     });
 
     it('renders another name', () => {
         const customer = { firstName: "Amber" };
-        // const component = (<Header customer={customer} />),
-        const component = <Header customer={customer} />,
-            container = document.createElement("div");
+        const component = <Header customer={customer} />;
 
-        document.body.replaceChildren(container);
         act(() => {
             ReactDOM.createRoot(container).render(component);
         });
+
         expect(document.body.textContent).toContain('Amber');
     });
 });
