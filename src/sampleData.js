34df0264c45759c1596460aaa1a863a18494c874
Eddie Lao <eddielao@gmail.com>
2023-03-08 12:53:11 -0500

    Add more job experiences

--- src/sampleData.js
+++ src/sampleData.js
@@ -4,11 +4,19 @@ const inYearMonth = (year, month) => monthYearDate.setFullYear(year, month);
 
 export const sampleExperiences = [
     {
-        startsIn: inYearMonth(2020, 10),
+        startsIn: inYearMonth(1999, 11),
         company: { fullName: 'United States Air Force' }
     },
     {
-        startsIn: inYearMonth(2021, 11),
+        startsIn: inYearMonth(2005, 5),
         company: { fullName: 'Vinculums' }
+    },
+    {
+        startsIn: inYearMonth(2014, 5),
+        company: { fullName: 'Blackwave Telecom' }
+    },
+    {
+        startsIn: inYearMonth(2017, 7),
+        company: { fullName: 'Wells Fargo' }
     }
 ];
