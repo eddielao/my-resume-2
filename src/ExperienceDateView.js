c4914be7818d7f498b467aa5bf18ec59a1da0631
Eddie Lao <eddielao@gmail.com>
2023-03-08 11:44:45 -0500

    Refactor ExperienceDateView component

--- src/ExperienceDateView.js
+++ src/ExperienceDateView.js
@@ -4,15 +4,26 @@ import { Experience } from "./Experience";
 
 export const ExperienceDateView = ({ experiences }) => {
 
-    const [selectedExperience, setSelectedExperience] = useState(0);
+    const latestExperienceIndex = 0;
+    const [selectedExperience, setSelectedExperience] = useState(latestExperienceIndex);
     const toMonthYear = startsAt => {
         const [m, y] = new Date(startsAt).toLocaleDateString('en-US',
             { year: 'numeric', month: 'short' }).toUpperCase().split(' ');
         return `${m} ${y}`;
     };
 
-    const experienceDateView =
-        experiences && experiences.length > 0 ?
+    const experienceList = experiences && experiences.length > 0 ?
+        experiences.map((element, index) =>
+            <li key={element.startsAt}>
+                <button onClick={() => setSelectedExperience(index)} type='button'>
+                    DateIcon: {toMonthYear(element.startsAt)}
+                </button>
+            </li>
+        )
+        :
+        "";
+
+    const experienceDateView = experiences && experiences.length > 0 ?
         <Experience experience={experiences[selectedExperience]} />
         :
         <p>There are no experiences.</p>;
@@ -20,13 +31,7 @@ export const ExperienceDateView = ({ experiences }) => {
     return (
         <div id="experienceDayView">
             <ol>
-                {experiences.map((element, index) =>
-                    <li key={element.startsAt}>
-                        <button onClick={() => setSelectedExperience(index)} type='button'>
-                            DateIcon: {toMonthYear(element.startsAt)}
-                        </button>
-                    </li>
-                )}
+                {experienceList}
             </ol>
             {experienceDateView}
         </div>
