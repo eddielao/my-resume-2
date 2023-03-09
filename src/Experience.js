8037cbbec788fba05ae5f5ed065ac44640b054a5
Eddie Lao <eddielao@gmail.com>
2023-03-08 19:05:05 -0500

    Add more experience data

--- src/Experience.js
+++ src/Experience.js
@@ -1,3 +1,7 @@
 import React from 'react';
 
-export const Experience = ({ experience }) => <div>{experience.company.fullName}</div>;
+export const Experience = ({ experience }) =>
+    <div>
+        <h3>{experience.title}</h3>
+        {experience.company.fullName}
+    </div>;
--- src/sampleData.js
+++ src/sampleData.js
@@ -8,19 +8,22 @@ class Company {
 }
 
 class Experience {
+    title;
     startsIn;
-    company;
-    constructor(year, month, company) {
+    company = new Company();
+    accomplishment = [];
+    constructor(year, month, company, title) {
         this.startsIn = this.inYearMonth(year, month);
         this.company = company;
+        this.title = title;
     }
 
     inYearMonth (year, month) { return new Date().setFullYear(year, month); }
 };
 
 export const sampleExperiences = [
-    new Experience(1999, 11, new Company('United States Air Force', 'USAF')),
-    new Experience(2005, 5, new Company('Vinculums')),
-    new Experience(2014, 5, new Company('Blackwave Telecom')),
-    new Experience(2017, 7, new Company('Wells Fargo'))
+    new Experience(1999, 11, new Company('United States Air Force', 'USAF'), 'Programming Specialist'),
+    new Experience(2005, 5, new Company('Vinculums'), 'Database Software Engineer'),
+    new Experience(2014, 5, new Company('Blackwave Telecom'), 'Database Software Engineer'),
+    new Experience(2017, 7, new Company('Wells Fargo'), 'Senior Software Engineer')
 ];
