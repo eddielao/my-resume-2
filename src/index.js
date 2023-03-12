8e6b1c06c6e8450cdbfad2c5f6b839e04689d786
Eddie Lao <eddielao@gmail.com>
2023-03-12 18:14:06 -0400

    Add sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import './style.scss';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ExperienceDateView } from './ExperienceDateView';
