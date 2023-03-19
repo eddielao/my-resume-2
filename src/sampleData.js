0ad5ce4aba888d232c1988394afcec298c3a05de
Eddie Lao <eddielao@gmail.com>
2023-03-19 11:06:39 -0400

    Add sample data for profile links

--- src/sampleData.js
+++ src/sampleData.js
@@ -2,6 +2,11 @@ import usaf from './media/usaf.png';
 import vinculums from './media/vinculums.png';
 import wf from './media/wf.png';
 
+const BOOTSTRAP_NUM_ICON = '-circle-fill';
+const
+    GITHUB_URL = 'https://github.com/eddielao',
+    LINKEDIN_URL = 'https://linkedin.com/in/eddielao';
+
 
 class Company {
     fullName;
@@ -28,9 +33,23 @@ class Experience {
     inYearMonth (year, month) { return new Date().setFullYear(year, month); }
 };
 
+class urlLink {
+    url;
+    bootstrapIcon;
+    constructor(icon, url=null) {
+        this.bootstrapIcon = icon;
+        this.url = url;
+    }
+}
+
 export const sampleExperiences = [
     new Experience(1999, 11, new Company('United States Air Force', 'USAF', usaf), 'Programming Specialist'),
     new Experience(2005, 5, new Company('Vinculums', 'V', vinculums), 'Database Engineer'),
     new Experience(2014, 5, new Company('Blackwave Telecom', 'BW'), 'Database Engineer'),
     new Experience(2017, 7, new Company('Wells Fargo', 'WF', wf), 'Software Engineer')
+];
+
+export const sampleUrlLink = [
+    new urlLink(BOOTSTRAP_NUM_ICON, GITHUB_URL),
+    new urlLink(BOOTSTRAP_NUM_ICON, LINKEDIN_URL)
 ];
