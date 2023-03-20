d54b366a85a451cdd130dc55aac6f67367eb6519
root <root@CodeM>
2023-03-20 16:05:02 -0400

    Create link to Google Form

--- src/Header.js
+++ src/Header.js
@@ -11,13 +11,20 @@ export const Header = () => {
                 <form className='form-inline'>
                     <div className='input-group'>
                         <DropdownMenu sampleUrlLink={sampleUrlLink} />
-                        <input
-                            type='text'
-                            className='form-control'
-                            placeholder='Eddie Lao'
-                            aria-label='CandidateName'
-                            disabled
-                        />
+                        <a
+                            href='https://forms.gle/xyphXMDzDaV3Ew5L8'
+                            target='_blank'
+                        >
+                            <input
+                                aria-label='CandidateName'
+                                className='form-control'
+                                data-bs-toggle='tooltip'
+                                
+                                placeholder='Eddie Lao'
+                                title='Click to contact me'
+                                type='text'
+                            />
+                        </a>
                     </div>
                 </form>
             </nav>
