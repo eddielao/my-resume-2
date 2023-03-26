8f331405167b3c7e5ad7a970c91a884772af7fb2
Eddie Lao <eddielao@gmail.com>
2023-03-26 17:26:29 -0400

    Switch Jest matcher from toBeTruthy to toContain

--- test/components/DropdownMenu.test.js
+++ test/components/DropdownMenu.test.js
@@ -18,12 +18,23 @@ describe('DropdownMenu component', () => {
         })
     };
 
-    it('renders 0 circle fill icon when prop sample URL link has 0 length', () => {
-        const emptySampleUrlLinkMock = [];
+    const sampleUrlLinkMock = [
+        { bootStrapIcon: 'mockIcon1', url: 'mockUrl1' },
+        { bootStrapIcon: 'mockIcon2', url: 'mockUrl2' },
+        { bootStrapIcon: 'mockIcon3', url: 'mockUrl3' },
+        { bootStrapIcon: 'mockIcon4', url: 'mockUrl4' },
+        { bootStrapIcon: 'mockIcon5', url: 'mockUrl5' }
+    ];
+
+    it('renders correct numbered circle fill icon when prop sample URL link has 0 length', () => {
+        const
+            emptySampleUrlLinkMock = [],
+            threeSampleUrlLinkMock = [sampleUrlLinkMock[0], sampleUrlLinkMock[1], sampleUrlLinkMock[2]],
+            fiveSampleUrlLinkMock = [sampleUrlLinkMock[0], sampleUrlLinkMock[1], sampleUrlLinkMock[2],
+                sampleUrlLinkMock[3], sampleUrlLinkMock[4]];
 
         render(<DropdownMenu sampleUrlLink={emptySampleUrlLinkMock} />);
-        const BSIcon = document.querySelector('i[class*="bi-0-circle-fill"]');
-
-        expect(BSIcon).toBeTruthy();
+        const BSIcon = document.querySelector('button > i');
+        expect(BSIcon.className).toContain('bi-0-circle-fill');
     });
 });
